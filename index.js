9b59e5778ac22cd67299dbb7a2b0e357174e42c6
Mahmod-Hassan <mahmodhasan7788@gmail.com>
2022-11-22 22:13:04 +0600

    create .env file and set datase user and password

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
+
 // middleware 
 app.use(cors());
 app.use(express.json());
