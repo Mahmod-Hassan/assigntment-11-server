627624d950793c5d0759ebe409adb9d6efb54c75
Mahmod-Hassan <mahmodhasan7788@gmail.com>
2022-11-22 22:17:05 +0600

    connect server with mongodb

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const app = express();
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lcblope.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 app.get('/', (req, res) => {
     res.send('service review server is running');
 })
